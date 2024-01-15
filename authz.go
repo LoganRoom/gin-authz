3eab74a528b9650ce3cc3b2174fb02577d9182ea
Rhys Morgan <morgan@Rhyss-MacBook-Pro-2.local>
2024-01-15 14:57:41 +0000

    removed basicauth methods and replaced with context based methods

--- authz.go
+++ authz.go
@@ -1,55 +1,62 @@
-// Copyright 2014 Manu Martinez-Almeida.  All rights reserved.
-// Use of this source code is governed by a MIT style
-// license that can be found in the LICENSE file.
-
 package authz
 
 import (
-	"net/http"
+    "net/http"
+    "strings"
 
-	"github.com/casbin/casbin/v2"
-	"github.com/gin-gonic/gin"
+    "github.com/casbin/casbin/v2"
+    "github.com/gin-gonic/gin"
 )
 
-// NewAuthorizer returns the authorizer, uses a Casbin enforcer as input
-func NewAuthorizer(e *casbin.Enforcer) gin.HandlerFunc {
-	a := &BasicAuthorizer{enforcer: e}
-
-	return func(c *gin.Context) {
-		if !a.CheckPermission(c.Request) {
-			a.RequirePermission(c)
-		}
-	}
+// CustomAuthorizer is a custom authorizer that checks roles in the Gin context for permissions.
+type CustomAuthorizer struct {
+    enforcer *casbin.Enforcer
 }
 
-// BasicAuthorizer stores the casbin handler
-type BasicAuthorizer struct {
-	enforcer *casbin.Enforcer
+// NewCustomAuthorizer returns a custom authorizer that uses a Casbin enforcer as input.
+func NewCustomAuthorizer(e *casbin.Enforcer) gin.HandlerFunc {
+    a := &CustomAuthorizer{enforcer: e}
+
+    return func(c *gin.Context) {
+        if !a.CheckPermission(c) {
+            a.RequirePermission(c)
+        }
+    }
 }
 
-// GetUserName gets the user name from the request.
-// Currently, only HTTP basic authentication is supported
-func (a *BasicAuthorizer) GetUserName(r *http.Request) string {
-	username, _, _ := r.BasicAuth()
-	return username
+// CheckPermission checks if the roles in the Gin context have permission for the requested URL and HTTP verb.
+func (a *CustomAuthorizer) CheckPermission(c *gin.Context) bool {
+    // Get roles from the Gin context, assuming roles are stored as a comma-delimited string
+    roles, exists := c.Get("roles")
+    if !exists {
+        // No roles found in the context, return false (permission denied)
+        return false
+    }
+
+    // Convert roles to a slice of strings
+    roleSlice := strings.Split(roles.(string), ",")
+
+    // Get the HTTP method and URL path from the Gin context
+    method := c.Request.Method
+    path := c.FullPath()
+
+    // Check each role for permission
+    for _, role := range roleSlice {
+        allowed, err := a.enforcer.Enforce(role, path, method)
+        if err != nil {
+            panic(err)
+        }
+        if allowed {
+            // If any role has permission, return true (permission granted)
+            return true
+        }
+    }
+
+    // If no role has permission, return false (permission denied)
+    return false
 }
 
-// CheckPermission checks the user/method/path combination from the request.
-// Returns true (permission granted) or false (permission forbidden)
-func (a *BasicAuthorizer) CheckPermission(r *http.Request) bool {
-	user := a.GetUserName(r)
-	method := r.Method
-	path := r.URL.Path
-
-	allowed, err := a.enforcer.Enforce(user, path, method)
-	if err != nil {
-		panic(err)
-	}
-
-	return allowed
-}
-
-// RequirePermission returns the 403 Forbidden to the client
-func (a *BasicAuthorizer) RequirePermission(c *gin.Context) {
-	c.AbortWithStatus(http.StatusForbidden)
+// RequirePermission returns the 403 Forbidden status to the client
+func (a *CustomAuthorizer) RequirePermission(c *gin.Context) {
+    c.AbortWithStatus(http.StatusForbidden)
 }
